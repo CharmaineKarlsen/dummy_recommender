2b5e1416f94e2b66aa6c680c92ea2e836244a9d7
CharmaineKarlsen <charmainekarlsen@gmx.de>
2022-02-23 14:29:19 +0100

    update to match D

--- recommender.py
+++ recommender.py
@@ -1,20 +1,11 @@
 """
-contains various implementations for recommending movies
+contains function for recommending random movies
 """
 
 import pandas as pd
-from utils import movies, ratings
 
-
-# recommender_systems_intro_filled.ipynb
-def recommend_random(query, ratings, k=10):
+def recommend_random(movies, k=10):
     """
     Recommends a list of k random movie ids
     """
-    return [1, 20, 34, 25]
-
-
-if __name__=='__main__':
-    # list of liked movies
-    query = [1, 34, 56, 21]
-    print(recommend_random(query, movies))
+    return movies['title'].sample(k).to_list()
