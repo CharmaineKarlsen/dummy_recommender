1372b43b6e59db13570eb7eb91ce5a77b99273fb
CharmaineKarlsen <charmainekarlsen@gmx.de>
2022-02-23 15:40:40 +0100

    adding test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+''' 
+testing test
+'''
+
+import pandas as pd
+from recommender import recommend_random
+
+# testing the recommend_random function to check length equals 10
+def test_recommender():
+    """
+    testing recommender
+    """
+    movies = pd.read_csv('./data/movies.csv')
+    recommendations = recommend_random(movies=movies,k=10)
+    assert len(reommendations)==10
+    
